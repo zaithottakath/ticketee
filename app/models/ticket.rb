bdac88dc6b11e3b20d42cad14604b8f0d0dd2143
zaithottakath <zaithottakath@gmail.com>
2013-03-10 20:11:16 -0700

    Implement creating tickets for a project

---
+++ app/models/ticket.rb
@@ -0,0 +1,11 @@
+class Ticket < ActiveRecord::Base
+  belongs_to :project
+  attr_accessible :description, :title
+
+  validates :title, :presence => true
+  validates :description, :presence => true, :length => { :minimum => 10 }
+  #creates the "Title (and Description) can't be blank" errors
+  #could also be
+  #validates :title, :description, :presence => true
+
+end
