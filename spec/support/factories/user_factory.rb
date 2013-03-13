3893cb13c156481b6fe210694a4e71c9445bef89
zaithottakath <zaithottakath@gmail.com>
2013-03-13 00:16:57 -0700

    Associate users to a ticket when tickets are created

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,7 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@ticketee.com"}
+    email "ticketee@email.com"
     password "password"
     password_confirmation "password"
+
+    factory :confirmed_user do
+      after_create do |user|
+        user.confirm!
+      end
+    end
   end
 end
