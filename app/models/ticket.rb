4a8662097d43d8ec679a7f3a291d305cb031ead6
zaithottakath <zaithottakath@gmail.com>
2013-03-20 11:16:57 -0700

    Users can now upload 3 files at a time

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,12 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
-  attr_accessible :description, :title, :asset
-  has_attached_file :asset
+
+  has_many :assets
+  accepts_nested_attributes_for :assets
+  attr_accessible :description, :title, :assets_attributes
+
+
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
   #creates the "Title (and Description) can't be blank" errors
